c94fd50afa85b8ee769913d1c6e549a3e19df5ab
Nikki162 <nikoestudoseafins@gmail.com>
2026-05-28 22:01:20 -0300

    terminei meus estudos hoje

---
+++ exercicios/Cap01/ifTest2.java
@@ -0,0 +1,13 @@
+package exercicios.Cap01;
+
+public class ifTest2 {
+    public static void main (String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x deve ser igual a 3");
+        } else {
+            System.out.println("x NÃO é igual a 3");
+        }
+        System.out.println("Isso será executado de qualquer forma");
+    }
+}
