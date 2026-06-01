705632b61aaae943d356419ef1cc41ddd9e240eb
Nikki162 <nikoestudoseafins@gmail.com>
2026-06-01 18:21:06 -0300

    Novos exercicios

---
+++ exercicios/Cap01/Exercise1b.java
@@ -0,0 +1,25 @@
+package exercicios.Cap01;
+
+public class Exercise1b {
+    public static void main (String [] args) {
+        int x = 1;
+        while ( x < 10 ) {
+        // a mudança foi apenas adicionar um valor a ser somado para X
+            if (x > 3) {
+                System.out.println("big x");
+            }
+
+            x = x + 1;
+        }
+        
+        System.out.println("--------------");
+
+        int y = 5;
+        while ( y > 1 ) {
+            x = x - 1;
+            if
+        }
+
+
+    }
+}
