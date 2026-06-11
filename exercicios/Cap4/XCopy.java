c6ebef17d8e4c339eeb48e9d28214e2139f62671
Nikki162 <nikoestudoseafins@gmail.com>
2026-06-11 09:27:47 -0300

    fiz exercicios do livro

---
+++ exercicios/Cap4/XCopy.java
@@ -0,0 +1,22 @@
+package exercicios.Cap4;
+
+class XCopy {
+    
+    public static void main(String[] args) {
+
+        int orig = 42;
+
+        XCopy x = new XCopy();
+
+        int y = x.go(orig);
+
+        System.out.println(orig + " " + y);
+    }
+
+    int go(int arg) {
+
+        arg = arg * 2; 
+
+        return arg; 
+    }
+}
